7beb56fe53d44b167a4f9c4096e779ff0708cdbc
Rodolfo Barrientos <rudybar12@yahoo.com>
2020-07-29 03:11:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if "I lOVE YOU GRANDMA"
+  if phrase == "I lOVE YOU GRANDMA"
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase = phrase.upcase
     "NO, NOT SINCE 1938!"
