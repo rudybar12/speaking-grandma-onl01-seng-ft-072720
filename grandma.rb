eb42cbce9af12f7c19827758170e49bb1f066d8a
Rodolfo Barrientos <rudybar12@yahoo.com>
2020-07-29 03:21:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  binding.pry
+  #binding.pry
   if phrase == "I LOVE YOU GRANDMA!"
      "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
