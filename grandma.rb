16902f230f8b368748c8ba9a7bfa3b03a28bdadf
Rodolfo Barrientos <rudybar12@yahoo.com>
2020-07-29 03:11:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I lOVE YOU GRANDMA"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase = phrase.upcase
+  elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
